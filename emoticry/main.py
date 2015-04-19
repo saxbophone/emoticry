8f047801101aa6e4c6efc14c47c330449cd09738
Joshua Saxby <saxbophone@users.noreply.github.com>
2015-04-19 14:47:04 +0100

    add command-line descriptions

--- emoticry/main.py
+++ emoticry/main.py
@@ -20,8 +20,22 @@ def main():
         description='Mercilessly translates file names to emoji',
         epilog=('This program must be used with extreme caution '
                 'as there is currently no way to revert filenames!'))
-    parser.add_argument('path', nargs='?', default='.')
-    parser.add_argument('-R', dest='recursive', action='store_true')
+
+    parser.add_argument('path',
+                        nargs='?',
+                        default='.',
+                        help=('Specifies the path to the directory where'
+                              'files and directories will be renamed. '
+                              '(If not given then defaults to the current '
+                              'directory).'))
+
+    parser.add_argument('-R',
+                        dest='recursive',
+                        action='store_true',
+                        help=('Recursively rename all files and directories. '
+                              '(Optional argument).'))
     parser.set_defaults(recursive=False)
+
     args = parser.parse_args()
+
     emoticry.emojify(args.path, args.recursive)
